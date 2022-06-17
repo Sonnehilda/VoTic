68fc3003efb4a29889cba912e32ca6f2a2db568b
__Rals <winhave@naver.com>
2022-06-17 20:21:07 +0900

    Added Footer Component.

---
+++ src/component/Footer/index.tsx
@@ -0,0 +1,20 @@
+import { css } from "@emotion/react";
+
+const Footer = () => {
+  return <footer css={backgroundStyle}></footer>;
+};
+
+const backgroundStyle = css`
+  background-color: #f1f1f1;
+
+  position: relative;
+
+  width: 100%;
+  height: 6rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default Footer;
