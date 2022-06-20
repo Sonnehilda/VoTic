359eda56ffb9b7b2e2720a20c64a9522c4af24d8
__Rals <winhave@naver.com>
2022-06-20 19:58:07 +0900

    Updated Footer Component.

--- src/component/Footer/index.tsx
+++ src/component/Footer/index.tsx
@@ -1,13 +1,19 @@
 import { css } from "@emotion/react";
 
 const Footer = () => {
-  return <footer css={backgroundStyle}></footer>;
+  return (
+    <footer css={backgroundStyle}>
+      <strong>ⓒ 2022 __Rals All rights reserved.</strong>
+      <span>Front-End : __Rals</span>
+      <span>Back-End : khcho0125</span>
+    </footer>
+  );
 };
 
 export default Footer;
 
 const backgroundStyle = css`
-  background-color: #f1f1f1;
+  background-color: #f6f6f6;
 
   position: relative;
 
@@ -15,6 +21,17 @@ const backgroundStyle = css`
   height: 6rem;
 
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  font-size: 1rem;
+
+  span {
+    font-size: 0.75rem;
+
+    :first-of-type {
+      margin-top: 0.5rem;
+    }
+  }
 `;
