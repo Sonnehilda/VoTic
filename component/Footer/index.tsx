c3d72d0f60c32244d80b920c76dd29ce815e9ee3
__Rals <winhave@naver.com>
2022-07-05 15:09:29 +0900

    Applied Theme Provider To Footer Component.

--- component/Footer/index.tsx
+++ component/Footer/index.tsx
@@ -1,8 +1,9 @@
 import { css } from "@emotion/react";
+import theme, { ThemeProps } from "../../styles/theme";
 
-const Footer = () => {
+const Footer = ({ themeId }: ThemeProps) => {
   return (
-    <footer css={backgroundStyle}>
+    <footer css={() => backgroundStyle(themeId)}>
       <strong>ⓒ 2022 __Rals All rights reserved.</strong>
       <span>Front-End : __Rals</span>
       <span>Back-End : khcho0125</span>
@@ -12,8 +13,8 @@ const Footer = () => {
 
 export default Footer;
 
-const backgroundStyle = css`
-  background-color: #f6f6f6;
+const backgroundStyle = (themeId: string) => css`
+  background-color: ${theme[themeId].background};
 
   position: relative;
 
@@ -25,6 +26,7 @@ const backgroundStyle = css`
   justify-content: center;
   align-items: center;
 
+  color: ${theme[themeId].color};
   font-size: 1rem;
 
   span {
@@ -33,9 +35,5 @@ const backgroundStyle = css`
     :first-of-type {
       margin-top: 0.5rem;
     }
-  }
-
-  @media (prefers-color-scheme: dark) {
-    background-color: #1a1a1a;
   }
 `;
