187cfac4f9ac10cf7632c75e9ef5f468cacf72b4
__Rals <winhave@naver.com>
2022-07-05 15:11:46 +0900

    Applied Theme Provider To Recent Votes Page.

--- pages/recent.tsx
+++ pages/recent.tsx
@@ -1,35 +1,66 @@
-import Footer from "../src/component/Footer";
-import Header from "../src/component/Header";
+import { useContext, useEffect, useState } from "react";
 import { css } from "@emotion/react";
-import LoginModal from "../src/component/LoginModal";
-import { useState } from "react";
-import PolicyModal from "../src/component/PolicyModal";
-import RegisterModal from "../src/component/RegisterModal";
-import FullViewList from "../src/component/FullViewList";
+import { ThemeColorContext } from "../context/Theme";
+import Header from "../component/Header";
+import Footer from "../component/Footer";
+import LoginModal from "../component/LoginModal";
+import PolicyModal from "../component/PolicyModal";
+import RegisterModal from "../component/RegisterModal";
+import FullViewList from "../component/FullViewList";
+import { NextRouter, useRouter } from "next/router";
+
+export default function Recent() {
+  const [modalState, setModalState] = useState<string>("");
+  const { themeColor, toggleThemeColor } = useContext(ThemeColorContext);
+
+  const router: NextRouter = useRouter();
+
+  useEffect(() => {
+    if (router.isReady) {
+      if (
+        localStorage.getItem("themeColor") &&
+        themeColor !== localStorage.getItem("themeColor")
+      )
+        toggleThemeColor();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.isReady]);
+
+  return (
+    <>
+      <div css={wrapper}>
+        {modalState === "login" && (
+          <LoginModal
+            setModalState={setModalState}
+            themeId={`${themeColor}0`}
+          />
+        )}
+        {modalState === "policy" && (
+          <PolicyModal
+            setModalState={setModalState}
+            themeId={`${themeColor}0`}
+          />
+        )}
+        {modalState === "register" && (
+          <RegisterModal
+            setModalState={setModalState}
+            themeId={`${themeColor}0`}
+          />
+        )}
+
+        <Header setModalState={setModalState} />
+        <FullViewList
+          title="최근에 생성된 투표"
+          type="recent"
+          themeId={`${themeColor}0`}
+        />
+      </div>
+      <Footer themeId={`${themeColor}0`} />
+    </>
+  );
+}
 
 const wrapper = css`
   height: auto;
   min-height: calc(100vh - 6rem);
 `;
-
-export default function Recent() {
-  const [modalState, setModalState] = useState<string>("");
-
-  return (
-    <>
-      <div css={wrapper}>
-        {modalState === "login" && <LoginModal setModalState={setModalState} />}
-        {modalState === "policy" && (
-          <PolicyModal setModalState={setModalState} />
-        )}
-        {modalState === "register" && (
-          <RegisterModal setModalState={setModalState} />
-        )}
-
-        <Header setModalState={setModalState} />
-        <FullViewList title="최근에 생성된 투표" type="recent" />
-      </div>
-      <Footer />
-    </>
-  );
-}
