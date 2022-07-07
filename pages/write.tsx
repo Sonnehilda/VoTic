6b1f945a5a7dad30cb54fd161b3733f764bd8ed4
__Rals <winhave@naver.com>
2022-07-07 12:07:55 +0900

    Added Write Page.

---
+++ pages/write.tsx
@@ -0,0 +1,76 @@
+import { useContext, useEffect, useRef, useState } from "react";
+import { css } from "@emotion/react";
+import { ThemeColorContext } from "../context/Theme";
+import Header from "../component/Header";
+import Footer from "../component/Footer";
+import LoginModal from "../component/LoginModal";
+import PolicyModal from "../component/PolicyModal";
+import RegisterModal from "../component/RegisterModal";
+import { NextRouter, useRouter } from "next/router";
+import WriteVote from "../component/WriteVote";
+import CreateOption from "../component/CreateOption";
+import AdviseModal from "../component/AdviseModal";
+
+export default function Write() {
+  const titleRef = useRef<HTMLInputElement>(null);
+
+  const [modalState, setModalState] = useState<string>("");
+  const { themeColor, toggleThemeColor } = useContext(ThemeColorContext);
+
+  const router: NextRouter = useRouter();
+
+  useEffect(() => {
+    if (router.isReady) {
+      if (
+        localStorage.getItem("themeColor") &&
+        themeColor !== localStorage.getItem("themeColor")
+      )
+        toggleThemeColor();
+      setModalState("advise");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.isReady]);
+
+  return (
+    <>
+      <Header setModalState={setModalState} themeId={`${themeColor}0`} />
+      <div css={wrapper}>
+        {modalState === "login" && (
+          <LoginModal
+            setModalState={setModalState}
+            themeId={`${themeColor}0`}
+          />
+        )}
+        {modalState === "policy" && (
+          <PolicyModal
+            setModalState={setModalState}
+            themeId={`${themeColor}0`}
+          />
+        )}
+        {modalState === "register" && (
+          <RegisterModal
+            setModalState={setModalState}
+            themeId={`${themeColor}0`}
+          />
+        )}
+        {modalState === "advise" && (
+          <AdviseModal
+            setModalState={setModalState}
+            themeId={`${themeColor}0`}
+          />
+        )}
+
+        <WriteVote titleRef={titleRef} themeId={`${themeColor}0`} />
+        <CreateOption titleRef={titleRef} themeId={`${themeColor}0`} />
+      </div>
+      <Footer themeId={`${themeColor}0`} />
+    </>
+  );
+}
+
+const wrapper = css`
+  padding-top: 3rem;
+
+  height: auto;
+  min-height: calc(100vh - 6rem);
+`;
