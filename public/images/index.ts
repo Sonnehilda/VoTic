806baa6fbd7f5c15fa23f3a59e319d6fda02b119
__Rals <winhave@naver.com>
2022-07-07 12:05:59 +0900

    Added Upload Image.

--- public/images/index.ts
+++ public/images/index.ts
@@ -3,3 +3,4 @@ export { default as search } from "./search.png";
 export { default as vote } from "./vote.png";
 export { default as sun } from "./sun.png";
 export { default as moon } from "./moon.png";
+export { default as upload } from "./upload.png";
