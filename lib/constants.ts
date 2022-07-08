ddab6d85f945255b5fe331c3e290e169d505d5bf
__Rals <winhave@naver.com>
2022-07-08 11:38:02 +0900

    Added General Constant File.

---
+++ lib/constants.ts
@@ -0,0 +1,71 @@
+// PreviewList Component & FullViewList Component.
+
+export const nameOfPopularityList = "인기 급상승 투표";
+export const nameOfRecentList = "최근에 생성된 투표";
+export const nameOfMineList = "당신이 생성한 투표";
+export const nameOfDefaultList = "모든 투표";
+
+// CreateOption Component & Register Modal Component.
+
+export const ValueNotExistException: string = "값이 입력되지 않았습니다!";
+export const TitleNotExistException: string =
+  "투표 제목이 입력되지 않았습니다!";
+export const OptionAlreadyExistsException: string =
+  "이미 해당 선택지가 존재합니다!";
+export const OptionNotFulfilledException: string =
+  "선택지를 최소 2개 만들어주세요!";
+export const SucceededMessage: string = "투표가 성공적으로 생성되었습니다!";
+
+// Register Modal Component.
+
+export const IdHasTakenException: string = "사용할 수 없는 아이디입니다.";
+export const PwDoesNotMatchException: string = "비밀번호가 일치하지 않습니다.";
+export const WrongEmailFormException: string =
+  "이메일 형식이 올바르지 않습니다.";
+export const EmailHasTakenException: string =
+  "해당 이메일로 가입된 회원이 존재합니다.";
+export const GeneralException: string = "알 수 없는 오류가 발생하였습니다.";
+export const VerifyCodeDoesNotMatchException: string =
+  "인증 코드가 올바르지 않습니다.";
+
+// Policy Modal Component & Advise Modal Component.
+
+export const iAgree: string = "위의 사항을 숙지했으며, 이에 동의합니다.";
+
+// Advise Modal Component.
+
+export const termsOfUseDiscretion: string[] = [
+  "서비스는 회원이 게시하거나 등록하는 서비스내의",
+  "내용물이 다음 각 호에 해당한다고 판단되는",
+  "경우에 사전통지 없이 삭제할 수 있습니다.",
+
+  "1. 다른 회원 또는 제삼자를 비방하거나",
+  "중상모략으로 명예를 손상시키는 내용인 경우",
+
+  "2. 범죄적 행위에 결부된다고 인정되는 내용일 경우",
+
+  "3. 회원이 음란물을 게재하거나",
+  "음란사이트를 링크하는 경우",
+
+  "4. 기타 관계법령에 위반된다고 판단되는 경우",
+
+  "회원이 서비스 내에 게시한 게시물에 대한",
+  "권리와 책임은 게시자에게 있습니다.",
+];
+
+// Policy Modal Component.
+
+export const purpose: string[] = [
+  "이용 고객 (또는 회원) 은(는) 서비스 회원가입,",
+  "고객 식별 및 고객 상담 등을 위해 ",
+  "아래와 같이 개인정보를 수집 및 이용합니다.",
+];
+
+export const privacyPolicyDiscretion: string[] = [
+  "개인정보 수집 및 이용에 대해서는 거부할 수 있으며,",
+  "거부 시에는 회원가입이 불가합니다.",
+  "서비스 제공을 위해서 반드시 필요한 최소한의 개인정보이므로",
+  "동의를 하셔야 서비스 이용이 가능합니다.",
+  "이 외 서비스 이용과정에서 별도 동의를 통해",
+  "추가정보 수집이 있을 수 있습니다.",
+];
