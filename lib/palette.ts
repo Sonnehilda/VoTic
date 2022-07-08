daa1cbe24d935a8683793939c9c4bcb8272849bb
__Rals <winhave@naver.com>
2022-07-08 11:38:33 +0900

    Added Palette.

---
+++ lib/palette.ts
@@ -0,0 +1,17 @@
+export const palette = {
+  light0: "#f6f6f6",
+  light1: "#efefef",
+  light2: "#e1e1e1",
+  light3: "#dfdfdf",
+  light4: "#d1d1d1",
+  light5: "#a1a1a1",
+
+  dark0: "#212121",
+  dark1: "#3f3f3f",
+  dark2: "#444",
+  dark3: "#4f4f4f",
+  dark4: "#555",
+  dark5: "#666",
+
+  main: "#00ffab",
+};
