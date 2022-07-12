8794175ecf4e76baa778bd1cc125482bdfb479cc
__Rals <winhave@naver.com>
2022-07-12 16:32:28 +0900

    Added Images.

--- public/images/index.ts
+++ public/images/index.ts
@@ -4,3 +4,5 @@ export { default as vote } from "./vote.png";
 export { default as sun } from "./sun.png";
 export { default as moon } from "./moon.png";
 export { default as upload } from "./upload.png";
+export { default as pencil } from "./pencil.png";
+export { default as check } from "./check.png";
